bd287374c2574f1a91eb08c760f1f8fbb227e10a
Raquel <raquelboujnah@gmail.com>
2024-07-07 15:38:46 +0300

    new day

--- week-5/day_3/xp_gold.js
+++ week-5/day_3/xp_gold.js
@@ -31,8 +31,33 @@ function changeCase(sentence){
         }
     }return swappedStr;
 }
-
 console.log(changeCase('The Quick Brown Fox'));
 
+// exercise 4
+// Create a function that determines whether an argument is omnipresent for a given array.
+// A value is omnipresent if it exists in every subarray inside the main array.
 
-    
+let arr = [[3, 4], [8, 3, 2], [3], [9, 3], [5, 3], [4, 3]]
+function isOmnipresent(array, value) {
+    for (let subarray of array) {
+        let found = false;
+        for (let item of subarray) {
+            if (item === value) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(isOmnipresent(arr, 2));
+
+
+// exercise 5
+let table = document.querySelector('table');
+for (let i = 0; i < table.rows.length; i++) {
+    table.rows[i].cells[i].style.backgroundColor = 'red';
+}
