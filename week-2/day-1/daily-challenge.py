630d02b90c420356d5f69892bd88ce9e4ae28adc
Raquel Boujnah <raquelboujnah@gmail.com>
2025-07-02 15:49:37 +0300

    Update daily-challenge.py

--- week-2/day-1/daily-challenge.py
+++ week-2/day-1/daily-challenge.py
@@ -1,13 +1,11 @@
 user_word = input("chose a password\n")
 if len(user_word) < 10:
     print("password too short")
-elif len(user_word) > 10:
-    print("password too long")
+
 elif len(user_word) == 10:
     print("perfect password")
     
 first_character = user_word[0]
-last_character = user_word[-1]
 print(f"your password is between {first_character} and {last_character}")
 print(user_word)
 
