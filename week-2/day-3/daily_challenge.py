89ebffe16ccf57515af08fb985b22420845c8635
Raquel Boujnah <raquelboujnah@gmail.com>
2025-07-21 16:50:19 +0300

    Update daily_challenge.py

--- week-2/day-3/daily_challenge.py
+++ week-2/day-3/daily_challenge.py
@@ -6,12 +6,9 @@
 # Make sure the letters are strings.
 # Make sure the indexes are stored in a list and those lists are values.
 # user_word = input("type a word")
-# word_list = {}
+# word_list = []
 # for index, letter in enumerate(user_word):
-#     if index in word_list:
-#         word_list[letter].append(index)
-#     else :
-#         word_list[letter] = [index]
+#     word_list[letter].append(index)
 # print(word_list)
 
 #challenge-2
@@ -24,38 +21,26 @@ items_purchase = {
   "Spoon": "$20"
 }
 wallet = input('how much would you like to spend? ')
-if "$" in wallet:
-    wallet = int(wallet.replace("$",""))
-else:
-    wallet = int(wallet)
-    
 items_list = []
 for item, price in items_purchase.items():
-    if "," in price:
-        price = price.replace(",", "")
-    convert_value = int(price.replace("$", ""))
-    if convert_value < wallet:
+    if convert_value > wallet:
         wallet -= convert_value
-        items_list.append(item)
-if len(items_list) == 0:
+        items_list = (item)
+if len(items_list) = 0:
     print("nothing")
 else:
-    items_list.sort()
     print(items_list)
     
 total_cost = 0
-num_people = int(input('How many people are in the family?'))
-for i in range(num_people):
-    age = int(input(f'Enter the age of person {i + 1}:'))
+num_people = input('How many people are in the family?')
+for i in num_people:
+    age = input(f'Enter the age of person {i}:')
     if age < 3:
         print('Ticket is free')
         ticket_price = 0
-    elif 3 <= age <= 12:
+    elif age 3 <= 12:
         print('Ticket costs $10')
         ticket_price = 10
-    else:
-        print('Ticket costs £15')
-        ticket_price = 15
 
 total_cost += ticket_price
 print(f'\nThe totalcost for the family ticket is: ${total_cost}')
