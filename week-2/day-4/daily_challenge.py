7aebc10ee8c0099da75c6961d810725d02f335d3
Raquel Boujnah <raquelboujnah@gmail.com>
2025-08-14 15:45:10 +0300

    Update daily_challenge.py

--- week-2/day-4/daily_challenge.py
+++ week-2/day-4/daily_challenge.py
@@ -1,26 +1,16 @@
-secret = """7i3
-Tsi
-h%x
-i #
-sM 
-$a 
-#t%
-^r!"""
+secret = [['7', 'i', '3'],
+ ['T', 's', 'i'],
+ ['h', '%', 'x'],
+ ['i', ' ', '#'],
+ ['s', 'M', ' '],
+ ['$', 'a', ' '],
+ ['#', 't', '%'],
+ ['^', 'r', '!']]
 
-def create_colums() :
-    secret_list = list(secret.split("\n"))
-    lst = []
-    for num in range(3) :
-        lst.append([char[num] for char in secret_list])
-
-    return lst
 
 def decode() :
-    secret = create_colums()
-    # print("secret list", secret)
     final = ""
-    for arr in secret :
-        for char in arr :
+    for char in secret :
             if str(char).isalpha() :
                     final += char
             else :
