6291558420e0003e16cade4aa4da57b8d9bdf423
Raquel Boujnah <raquelboujnah@gmail.com>
2025-08-14 15:22:09 +0300

    Update xp_exercise.py

--- week-3/day-3/xp_exercise.py
+++ week-3/day-3/xp_exercise.py
@@ -1,51 +1,51 @@
-# #exercise-1
-# class Currency:
-#     def __init__(self, currency, amount):
-#         self.currency = currency
-#         self.amount = amount
+#exercise-1
+class Currency:
+    def __init__(self, currency, amount):
+        self.currency = currency
+        self.amount = amount
         
-#     def __str__(self):
-#         return f"{self.amount} {self.currency}"
+    def __str__(self):
+        return f"{self.amount} {self.currency}"
         
-#     def __int__(self):
-#         return self.amount
+    def __int__(self):
+        return self.amount
     
-#     def __repr__(self):
-#         return f"curency : {self.currency} amount : {self.amount}"
+    def __repr__(self):
+        return f"curency : {self.currency} amount : {self.amount}"
     
-#     def __add__(self, other):
-#         try:
-#             if self.currency == other.currency:
-#                return self.amount + other.amount
-#             else:
-#                 TypeError("Cannot add between Currency type <self.currency> and <other.currency>")
-#         except:
-#             return self.amount + other
+    def __add__(self, other):
+        try:
+            if self.currency == other.currency:
+               return self.amount + other.amount
+            else:
+                TypeError("Cannot add between Currency type <self.currency> and <other.currency>")
+        except:
+            return self.amount + other
     
-#     def __iadd__(self, other):
-#         try:
-#             self.amount += other.amount
-#             return self
-#         except:
-#             self.amount += other
-#             return self
+    def __iadd__(self, other):
+        try:
+            self.amount += other.amount
+            return self
+        except:
+            self.amount += other
+            return self
     
 
-# c1 = Currency('dollar', 5)
-# c2 = Currency('dollar', 10)
-# c3 = Currency('shekel', 1)
-# c4 = Currency('shekel', 10)
-# c1.__str__()
-# print(c1.__int__())
-# print(c1 + c2)
-# print(c1 + 2)
-# print(c1.amount)
-# c1 += 5
-# print(c1.amount)
+c1 = Currency('dollar', 5)
+c2 = Currency('dollar', 10)
+c3 = Currency('shekel', 1)
+c4 = Currency('shekel', 10)
+c1.__str__()
+print(c1.__int__())
+print(c1 + c2)
+print(c1 + 2)
+print(c1.amount)
+c1 += 5
+print(c1.amount)
 
 # #exercise 2
-# import func
-# func.add_two(1, 2)
+import func
+func.add_two(1, 2)
 
 #exercise 3
 import string
